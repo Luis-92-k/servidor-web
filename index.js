c3d78659affa8fcc7dd4ea6657998cffb7b2f6b6
Luis Velazquez <kurogane_miku@hotmail.com>
2021-01-11 17:20:29 -0600

    servidor web sin morgan

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 let http = require('http');
-let morgan = require('morgan');
 
-let logger = morgan('tiny');
 
 
 http.createServer((request, response) => {
-    logger(request, response, (err) => {
-        if (err) return done (err)
-
         if(request.url === "/contacto"){
             response.setHeader("Content-type", "text/html; charset=utf-8");
             response.write("<h1>Bienvenido a mi página de contacto</h1>");
             response.end();
-
         }else if(request.url === "/"){
             response.setHeader("Content-type", "text/html; charset=utf-8");
             response.write("<h1>Página de inicio</h1>");
@@ -22,7 +16,4 @@ http.createServer((request, response) => {
             response.write("<h1>404</h1>");
             response.end();
         }
-
-        
-    });
 }).listen(8080);
