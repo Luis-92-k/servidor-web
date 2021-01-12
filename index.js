bfededf944b80e1a57750df6e0dd1a1c2efddad6
Luis Velazquez <kurogane_miku@hotmail.com>
2021-01-12 15:58:32 -0600

    tarea casi lista

--- index.js
+++ index.js
@@ -4,22 +4,35 @@ let fs = require("fs");
 
 http.createServer((request, response) => {
     response.setHeader("Content-type", "text/html; charset=utf-8");
-        if(request.url === "/contacto"){
-            fs.readFile("./contact.html", (error, content) => {
-                if(!error){
-                    response.write(content);
-                    response.end();
-                }else{
-                    response.write("<h1>404</h1>");
-                    response.end();
-                }
-            });
-            
-        }else if(request.url === "/"){
-            response.write("<h1>Página de inicio</h1>");
-            response.end();
-        }else{
-            response.write("<h1>404</h1>");
+    switch(request.url){
+        case "/":
+            readFile("./index.html", response);
+            break;
+        case "/nosotros":
+            readFile("./about.html", response);
+            break;
+        case "/proyectos":
+            readFile("./projects.html", response);
+            break;
+        case "/contacto":
+            readFile("./contact.html", response);
+            break;
+        case "favicon.ico":
+            readFile("./favicon.ico", response);
+            break;
+        default:
+            readFile("./404.html", response);
+            break;
+    }
+
+}).listen(8080);
+
+
+const readFile = (path, response) => {
+    fs.readFile(path, (error, content) =>{
+        if(!error){
+            response.write(content);
             response.end();
         }
-}).listen(8080);
+    })
+}
